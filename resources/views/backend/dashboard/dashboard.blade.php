2ea0861cc71238613be89a68b3ddb4c4794a91cb
Hasibul10884 <hasibulhasan193@gmail.com>
2022-07-23 08:29:56 -0700

    dd

---
+++ resources/views/backend/dashboard/dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('backend.master') 
+@section('backend_content')
+
+@endsection
---
+++ resources/views/backend/fixed/sidebar.blade.php
@@ -0,0 +1,407 @@
+<div class="dlabnav">
+            <div class="dlabnav-scroll">
+                <ul class="metismenu" id="menu">
+                    <li class="dropdown header-profile">
+                        <a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown">
+                            <img src="{{url('backend/images/photo.jpg')}}" width="20" alt="">
+                            <div class="header-info ms-3">
+                                <span class="font-w600 ">Hi,<b>Hasib</b></span>
+                                <small class="text-end font-w400">hasibulhasan193@gmail.com</small>
+                            </div>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end">
+                            <a href="app-profile.html" class="dropdown-item ai-icon">
+                                <svg id="icon-user1" xmlns="http://www.w3.org/2000/svg" class="text-primary" width="18"
+                                    height="18" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                    <circle cx="12" cy="7" r="4"></circle>
+                                </svg>
+                                <span class="ms-2">Profile </span>
+                            </a>
+                            <a href="email-inbox.html" class="dropdown-item ai-icon">
+                                <svg id="icon-inbox" xmlns="http://www.w3.org/2000/svg" class="text-success" width="18"
+                                    height="18" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path
+                                        d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
+                                    </path>
+                                    <polyline points="22,6 12,13 2,6"></polyline>
+                                </svg>
+                                <span class="ms-2">Inbox </span>
+                            </a>
+                            <a href="page-error-404.html" class="dropdown-item ai-icon">
+                                <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18"
+                                    height="18" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+                                    <polyline points="16 17 21 12 16 7"></polyline>
+                                    <line x1="21" y1="12" x2="9" y2="12"></line>
+                                </svg>
+                                <span class="ms-2">Logout </span>
+                            </a>
+                        </div>
+                    </li>
+                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                            <i class="flaticon-025-dashboard"></i>
+                            <span class="nav-text">Dashboard</span>
+                        </a>
+                    </li>
+
+<li>
+    <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+        <i class="fas fa-user"></i>
+        <span class="nav-text">Admin Manage</span>
+    </a>
+    <ul>
+        <li>
+            <a>
+                <i class="fa fa-circle"></i>
+                <span>Admin List</span>
+            </a>
+        </li>
+        <li>
+            <a>
+                <i class="fa fa-circle"></i>
+                <span>Add Admin</span>
+            </a>
+        </li>
+        <li>
+            <a>
+                <i class="fa fa-circle"></i>
+                <span>Roles</span>
+            </a>
+        </li>
+    </ul>
+</li>
+
+
+<li>
+    <a class="has-arrow">
+        <i class="fas fa-list"></i>
+        <span>Category and Brand</span>
+    </a>
+    <ul>
+    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Category</a>
+                                <ul aria-expanded="false">
+                                    <li><a href="email-compose.html">Add category</a></li>
+                                    <li><a href="email-inbox.html">Category List</a></li>
+                                    <li><a href="email-read.html">Edit Category</a></li>
+                                </ul>
+                            </li>
+                            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Brand</a>
+                                <ul aria-expanded="false">
+                                    <li><a href="email-compose.html">Add Brand</a></li>
+                                    <li><a href="email-inbox.html">Brand List</a></li>
+                                    <li><a href="email-read.html">Edit Brand</a></li>
+                                </ul>
+                            </li>
+    </ul>
+</li>
+<li>
+    <a class="has-arrow">
+        <i class="fab fa-product-hunt"></i>
+        <span>Products</span>
+    </a>
+    <ul>
+        <li>
+            <a>
+            <i class="fa-brands fa-product-hunt"></i>
+                <span>Add Product</span>
+            </a>
+        </li>
+        <li>
+            <a>
+            <i class="fa-brands fa-product-hunt"></i>
+                <span>Product List</span>
+            </a>
+        </li>
+        <li>
+            <a>
+            <i class="fa-brands fa-product-hunt"></i>
+                <span>Product Color</span>
+            </a>
+        </li>
+        <li>
+            <a>
+            <i class="fa-brands fa-product-hunt"></i>
+                <span>Product Size</span>
+            </a>
+        </li>
+    </ul>
+</li>
+
+
+
+        <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+        <i class="flaticon-041-graph"></i>
+                            <span class="nav-text">Order Management</span>
+                        </a>
+                        <ul aria-expanded="false">
+                       
+<ul>
+<li>
+<a>
+<span class="tio-circle nav-indicator-icon"></span>
+<span class="text-truncate">
+All
+<span class="badge badge-info badge-pill ml-1">
+21
+</span>
+ </span>
+</a>
+</li>
+<li>
+<a>
+<span class="tio-circle nav-indicator-icon"></span>
+<span class="text-truncate">
+Pending
+<span class="badge badge-soft-info badge-pill ml-1">
+10
+</span>
+</span>
+</a>
+</li>
+<li>
+<a>
+<span class="tio-circle nav-indicator-icon"></span>
+<span class="text-truncate">
+Confirmed
+<span class="badge badge-soft-success badge-pill ml-1">
+4
+</span>
+</span>
+</a>
+</li>
+<li>
+<a>
+<span class="tio-circle nav-indicator-icon"></span>
+<span class="text-truncate">
+Processing
+<span class="badge badge-warning badge-pill ml-1">
+1
+</span>
+</span>
+</a>
+</li>
+<li>
+<a>
+<span class="tio-circle nav-indicator-icon"></span>
+<span class="text-truncate">
+Out for delivery
+<span class="badge badge-warning badge-pill ml-1">
+0
+</span>
+</span>
+</a>
+</li>
+<li>
+<a>
+<span class="tio-circle nav-indicator-icon"></span>
+<span class="text-truncate">
+Delivered
+<span class="badge badge-success badge-pill ml-1">
+4
+</span>
+</span>
+</a>
+</li>
+ <li class="nav-item ">
+<a >
+<span class="tio-circle nav-indicator-icon"></span>
+<span class="text-truncate">
+Returned
+<span class="badge badge-soft-danger badge-pill ml-1">
+0
+</span>
+</span>
+</a>
+</li>
+<li>
+<a>
+<span class="tio-circle nav-indicator-icon"></span>
+<span class="text-truncate">
+Failed
+<span class="badge badge-danger badge-pill ml-1">
+2
+</span>
+</span>
+</a>
+</li>
+<li>
+<a>
+<span class="tio-circle nav-indicator-icon"></span>
+<span class="text-truncate">
+Canceled
+<span class="badge badge-soft-dark badge-pill ml-1">
+0
+</span>
+</span>
+</a>
+</li>
+</ul>
+</li>
+
+
+                            
+                        </ul>
+                    </li>
+                    </li>
+                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                            <i class="flaticon-050-info"></i>
+                            <span class="nav-text">Apps</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="app-profile.html">Profile</a></li>
+                            <li><a href="post-details.html">Post Details</a></li>
+                            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Email</a>
+                                <ul aria-expanded="false">
+                                    <li><a href="email-compose.html">Compose</a></li>
+                                    <li><a href="email-inbox.html">Inbox</a></li>
+                                    <li><a href="email-read.html">Read</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="app-calender.html">Calendar</a></li>
+                            
+                        </ul>
+                    </li>
+
+                     
+
+
+<li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                            <i class="flaticon-025-dashboard"></i>
+                            <span class="nav-text">Transactions</span>
+                        </a>
+                             </li>
+
+                             <li><a>
+<i class="tio-gift nav-icon"></i>
+<span class="navbar-vertical-aside-mini-mode-hidden-elements text-truncate">Coupon</span>
+</a>
+</li>
+
+
+
+<li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                            
+                            <i class="fas fa-users"></i>
+<span>Users</span>
+</a>
+ <ul>
+<li>
+<a>
+<i class="fa fa-circle"></i>
+<span>Customer List</span>
+</a>
+</li>
+</ul>
+                        </a>
+                    </li>
+
+
+
+                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                            <i class="flaticon-041-graph"></i>
+                            <span class="nav-text">Charts</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="chart-flot.html">Flot</a></li>
+                            <li><a href="chart-morris.html">Morris</a></li>
+                            <li><a href="chart-chartjs.html">Chartjs</a></li>
+                            <li><a href="chart-chartist.html">Chartist</a></li>
+                            <li><a href="chart-sparkline.html">Sparkline</a></li>
+                            <li><a href="chart-peity.html">Peity</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                            <i class="flaticon-086-star"></i>
+                            <span class="nav-text">Bootstrap</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="ui-accordion.html">Accordion</a></li>
+                            <li><a href="ui-alert.html">Alert</a></li>
+                            <li><a href="ui-badge.html">Badge</a></li>
+                            <li><a href="ui-button.html">Button</a></li>
+                            <li><a href="ui-modal.html">Modal</a></li>
+                            <li><a href="ui-button-group.html">Button Group</a></li>
+                            <li><a href="ui-list-group.html">List Group</a></li>
+                            <li><a href="ui-card.html">Cards</a></li>
+                            <li><a href="ui-carousel.html">Carousel</a></li>
+                            <li><a href="ui-dropdown.html">Dropdown</a></li>
+                            <li><a href="ui-popover.html">Popover</a></li>
+                            <li><a href="ui-progressbar.html">Progressbar</a></li>
+                            <li><a href="ui-tab.html">Tab</a></li>
+                            <li><a href="ui-typography.html">Typography</a></li>
+                            <li><a href="ui-pagination.html">Pagination</a></li>
+                            <li><a href="ui-grid.html">Grid</a></li>
+
+                        </ul>
+                    </li>
+                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                            <i class="flaticon-045-heart"></i>
+                            <span class="nav-text">Plugins</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="uc-select2.html">Select 2</a></li>
+                            <li><a href="uc-nestable.html">Nestedable</a></li>
+                            <li><a href="uc-noui-slider.html">Noui Slider</a></li>
+                            <li><a href="uc-sweetalert.html">Sweet Alert</a></li>
+                            <li><a href="uc-toastr.html">Toastr</a></li>
+                            <li><a href="map-jqvmap.html">Jqv Map</a></li>
+                            <li><a href="uc-lightgallery.html">Light Gallery</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="widget-basic.html" class="ai-icon" aria-expanded="false">
+                            <i class="flaticon-013-checkmark"></i>
+                            <span class="nav-text">Widget</span>
+                        </a>
+                    </li>
+                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                            <i class="flaticon-072-printer"></i>
+                            <span class="nav-text">Forms</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="form-element.html">Form Elements</a></li>
+                            <li><a href="form-wizard.html">Wizard</a></li>
+                            <li><a href="form-ckeditor.html">CkEditor</a></li>
+                            <li><a href="form-pickers.html">Pickers</a></li>
+                            <li><a href="form-validation.html">Form Validate</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                            <i class="flaticon-043-menu"></i>
+                            <span class="nav-text">Table</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="table-bootstrap-basic.html">Bootstrap</a></li>
+                            <li><a href="table-datatable-basic.html">Datatable</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                            <i class="flaticon-022-copy"></i>
+                            <span class="nav-text">Pages</span>
+                        </a>
+                        <ul aria-expanded="false">
+                            <li><a href="page-login.html">Login</a></li>
+                            <li><a href="page-register.html">Register</a></li>
+                            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Error</a>
+                                <ul aria-expanded="false">
+                                    <li><a href="page-error-400.html">Error 400</a></li>
+                                    <li><a href="page-error-403.html">Error 403</a></li>
+                                    <li><a href="page-error-404.html">Error 404</a></li>
+                                    <li><a href="page-error-500.html">Error 500</a></li>
+                                    <li><a href="page-error-503.html">Error 503</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="page-lock-screen.html">Lock Screen</a></li>
+                            <li><a href="empty-page.html">Empty Page</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class="copyright">
+                    <p><strong>Dompet Payment Admin Dashboard</strong> © 2022 All Rights Reserved</p>
+                    <p class="fs-12">Made with <span class="heart"></span> by DexignLab</p>
+                </div>
+            </div>
+        </div>
